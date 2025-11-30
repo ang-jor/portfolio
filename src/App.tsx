e564855731404c934f4453a66ff13646a68897d9
ang-jor <ang.j8r@gmail.com>
2025-11-30 18:02:26 +0100

    changes to container and adding some content

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,60 @@
 import "./App.css";
 import Container from "./components/Container.tsx";
-import ContainerWithTitle from "./components/ContainerWithTitle.tsx";
-import ImageContainer from "./components/ImageContainer.tsx";
+
+const introContent = (
+  <div className="welcome-content">
+    <h1>hi, i'm angela</h1>
+    <span>ux engineer @ skopje, macedonia</span>
+  </div>
+);
 
 function App() {
   return (
     <main>
       <div className="col">
         <div className="row">
-          <ImageContainer
+          <Container
+            hasImage
             content={
               <img src="../public/avatar.png" alt="me" className="avatar-img" />
             }
           />
-          <Container content="Hello World" />
+          <Container bgColor="#CAE7B9" content={introContent} className="intro-container"/>
         </div>
+        <Container
+          hasTitle
+          title="about me"
+          bgColor="#F3DE8A"
+          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida est non odio blandit, ut consequat metus interdum. Mauris sodales, velit vitae ornare condimentum, metus lacus interdum ligula, sed dapibus risus dolor sit amet purus. Quisque volutpat eros eget metus aliquam mattis. Phasellus bibendum lobortis nunc, vitae luctus lorem finibus eu. "
+        />
+        <Container
+          hasTitle
+          title="experience"
+          bgColor="#EB9486"
+          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida est non odio blandit, ut consequat metus interdum. Mauris sodales, velit vitae ornare condimentum, metus lacus interdum ligula, sed dapibus risus dolor sit amet purus. Quisque volutpat eros eget metus aliquam mattis. Phasellus bibendum lobortis nunc, vitae luctus lorem finibus eu. "
+        />
       </div>
       <div className="col">
-        <ContainerWithTitle
-          title="about me"
+        <Container
+          hasTitle
+          title="projects"
+          bgColor="#F3DE8A"
           content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida est non odio blandit, ut consequat metus interdum. Mauris sodales, velit vitae ornare condimentum, metus lacus interdum ligula, sed dapibus risus dolor sit amet purus. Quisque volutpat eros eget metus aliquam mattis. Phasellus bibendum lobortis nunc, vitae luctus lorem finibus eu. "
         />
-        <ContainerWithTitle
-          title="about me"
-          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida est non odio blandit, ut consequat metus interdum. Mauris sodales, velit vitae ornare condimentum, metus lacus interdum ligula, sed dapibus risus dolor sit amet purus. Quisque volutpat eros eget metus aliquam mattis. Phasellus bibendum lobortis nunc, vitae luctus lorem finibus eu. "
-        />
+        <div className="row">
+          <Container
+            hasTitle
+            title="get in touch"
+            bgColor="GREY"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida est non odio blandit, ut consequat metus interdum. Mauris sodales, velit vitae ornare condimentum, metus lacus interdum ligula, sed dapibus risus dolor sit amet purus. Quisque volutpat eros eget metus aliquam mattis. Phasellus bibendum lobortis nunc, vitae luctus lorem finibus eu. "
+          />
+          <Container
+            hasTitle
+            title="random"
+            bgColor="GREY"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida est non odio blandit, ut consequat metus interdum. Mauris sodales, velit vitae ornare condimentum, metus lacus interdum ligula, sed dapibus risus dolor sit amet purus. Quisque volutpat eros eget metus aliquam mattis. Phasellus bibendum lobortis nunc, vitae luctus lorem finibus eu. "
+          />
+        </div>
       </div>
     </main>
   );
--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,11 +2,29 @@ import React from "react";
 
 export interface ContainerProps {
   content: string | React.ReactElement;
+  hasTitle?: boolean;
+  hasImage?: boolean;
+  title?: string;
+  bgColor?: string;
+  className?: string;
 }
+
+const containerClass = (props: ContainerProps) => {
+  if (props.hasTitle) {
+    return "container-with-title";
+  } else if (props.hasImage) {
+    return "img-container";
+  }
+  return "container";
+};
 
 const Container = (props: ContainerProps) => {
   return (
-    <div className="container">
+    <div
+      className={`${containerClass(props)} ${props.className || ""}`}
+      style={{ backgroundColor: props.bgColor }}
+    >
+      {props.hasTitle && <h2>{props.title}</h2>}
       {typeof props.content === "string" ? (
         <p>{props.content}</p>
       ) : (
